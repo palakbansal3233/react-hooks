b2651c9060dd7dd634a606f125aa4fc1cc78db62
Palak Bansal <palakbansal3233@gmail.com>
2023-05-22 12:14:06 +0530

    Mounting | Cleanup after Unmount

--- src/App.js
+++ src/App.js
@@ -2,27 +2,21 @@ import React, { useState, useEffect } from 'react';
 
 const App = () => {
   
-  const [resourceType, setResourceType] = useState('posts');
-  const [items, setItems] = useState([]);
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
+  const handleResize = () => {
+    setWindowWidth(window.innerWidth);
+  }
   useEffect(() => {
-    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
-    .then(response => response.json())
-    .then(json => setItems(json))
-  }, [resourceType])
+    window.addEventListener('resize', handleResize)
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   return (
-    <>
-      <div>
-        <button onClick={() => setResourceType('posts')}>Posts</button>
-        <button onClick={() => setResourceType('users')}>Users</button>
-        <button onClick={() => setResourceType('comments')}>Comments</button>
-      </div>
-      <h1>{resourceType}</h1>
-      {items.map(item => {
-        return <pre>{JSON.stringify(item)}</pre>
-      })}
-    </>
+      <div>{windowWidth}</div>
   )
 }
 
